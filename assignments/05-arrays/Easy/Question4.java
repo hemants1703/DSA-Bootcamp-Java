58b7169375bb238efc437c554dd7df68b2cdfbae
Hemant Sharma <58618028+hemants1703@users.noreply.github.com>
2023-01-23 21:42:09 +0530

    Solved Question 4

---
+++ assignments/05-arrays/Easy/Question4.java
@@ -0,0 +1,22 @@
+//  https://leetcode.com/problems/richest-customer-wealth/
+public class Question4 {
+    public static void main(String[] args) {
+        int[][] accounts = {
+                {1,5},
+                {7,3},
+                {3,5}
+        };
+        System.out.println(maximumWealth(accounts));
+    }
+    public static int maximumWealth(int[][] accounts) {
+        int maximumWealth = 0, wealthCount = 0;
+        for(int customer = 0; customer < accounts.length; customer++) {
+            for(int account = 0; account < accounts[customer].length; account++)
+                wealthCount += accounts[customer][account];
+            if(wealthCount > maximumWealth)
+                maximumWealth = wealthCount;
+            wealthCount = 0;
+        }
+        return maximumWealth;
+    }
+}
