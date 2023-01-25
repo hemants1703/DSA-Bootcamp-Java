8313eb04bc75306aff011736c0c3548d3132d6e9
Hemant Sharma <58618028+hemants1703@users.noreply.github.com>
2023-01-25 15:07:54 +0530

    Solved Question 9

---
+++ assignments/05-arrays/Easy/Question9.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+//  https://leetcode.com/problems/create-target-array-in-the-given-order/
+public class Question9 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4};
+        int[] arr2 = {0,1,2,2,1};
+        System.out.println(Arrays.toString(createTargetArray(arr, arr2)));
+    }
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < nums.length; i++)
+            list.add(index[i], nums[i]);
+        int[] arr = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            arr[i] = list.get(i);
+        return arr;
+    }
+}
