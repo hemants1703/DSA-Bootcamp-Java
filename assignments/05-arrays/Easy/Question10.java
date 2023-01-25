07fb8d54c8f118e81b4270f4a8f65fd7f3657eb9
Hemant Sharma <58618028+hemants1703@users.noreply.github.com>
2023-01-25 15:21:46 +0530

    Solved Question 10

---
+++ assignments/05-arrays/Easy/Question10.java
@@ -0,0 +1,25 @@
+//  https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+public class Question10 {
+    public static void main(String[] args) {
+        System.out.println(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+    }
+    public static boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26)
+            return false;
+        boolean check = true;
+        for(int i = 65; i < 91; i++) {
+            char a = (char)i;           //  Uppercase Alphabets
+            char b = (char)(i + 32);    //  Lowercase Alphabets
+            for(int j = 0; j < sentence.length(); j++) {
+                if(sentence.charAt(j) == a || sentence.charAt(j) == b) {
+                    check = true;
+                    break;
+                }
+                else check = false;
+            }
+            if(!check)
+                return false;
+        }
+        return check;
+    }
+}
